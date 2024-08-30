50a44ddcfe6f84ba268f2356063ede5f1777ebbc
Almin5000 <71121668+Almin5000@users.noreply.github.com>
2024-08-30 11:49:22 +0200

    css for filter menu og filter knap

--- my-app/src/component/Home.jsx
+++ my-app/src/component/Home.jsx
@@ -3,61 +3,142 @@ import axios from 'axios';
 import LoginModal from './LoginModal';
 import '../css/home.css';
 import Footer from './Footer';
-import ProductGallery from './ProductGallery'
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import ProductGallery from './ProductGallery';
 
 function Home() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [products, setProducts] = useState([]);
-  const [sortOption, setSortOption] = useState('');
+  const [brands, setBrands] = useState([]);
+  const [genders, setGenders] = useState([]);
+  const [isFilterOpen, setFilterOpen] = useState(false);
+  const [filters, setFilters] = useState({
+    gender: [],
+    brand: [],
+    newest: false,
+    highToLow: false,
+    lowToHigh: false,
+  });
 
   const toggleModal = () => {
     setModalOpen(!isModalOpen);
   };
 
-  const handleSortChange = (e) => {
-    setSortOption(e.target.value);
+  const toggleFilter = () => {
+    setFilterOpen(!isFilterOpen);
+  };
+
+  const clearFilters = () => {
+    setFilters({
+      gender: [],
+      brand: [],
+      newest: false,
+      highToLow: false,
+      lowToHigh: false,
+    });
+    setSearchQuery('');
+  };
+
+  const handleCheckboxChange = (e) => {
+    const { name, value, checked } = e.target;
+
+    setFilters((prevFilters) => {
+      if (name === 'gender') {
+        const updatedGender = checked
+          ? [...prevFilters.gender, value]
+          : prevFilters.gender.filter(g => g !== value);
+        return {
+          ...prevFilters,
+          gender: updatedGender,
+        };
+      }
+
+      if (name === 'brand') {
+        const updatedBrand = checked
+          ? [...prevFilters.brand, value]
+          : prevFilters.brand.filter(b => b !== value);
+        return {
+          ...prevFilters,
+          brand: updatedBrand,
+        };
+      }
+
+      if (name === 'sort') {
+        return {
+          ...prevFilters,
+          [value]: checked,
+        };
+      }
+
+      return {
+        ...prevFilters,
+        [name]: checked,
+      };
+    });
   };
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const productResponse = await axios.get('https://hf6svendeapi-d5ebbcchbdcwcybq.northeurope-01.azurewebsites.net/api/Listing/verified');
-
+        
+        console.log('Fetched Products:', productResponse.data);
+  
         setProducts(productResponse.data);
+
+        const uniqueBrands = [...new Set(productResponse.data.map(product => product.product.brand))];
+        console.log('Unique Brands:', uniqueBrands);
+        setBrands(uniqueBrands);
+
+        const uniqueGenders = [...new Set(productResponse.data.map(product => product.product.gender))];
+        console.log('Unique Genders:', uniqueGenders);
+        setGenders(uniqueGenders);
+  
       } catch (err) {
         console.error('API Error:', err.message);
       }
     };
-
+  
     fetchProducts();
   }, []);
+  
+  const filteredProducts = products
+    .filter(product => {
+      const searchQueryLower = searchQuery.toLowerCase();
+      const matchesSearchQuery = 
+        product.title?.toLowerCase().includes(searchQueryLower) ||
+        product.product.brand?.toLowerCase().includes(searchQueryLower) ||
+        product.customerName?.toLowerCase().includes(searchQueryLower);
 
-  const sortedProducts = () => {
-    let sorted = [...products];
+      const matchesGender = !filters.gender.length || filters.gender.includes(product.product.gender);
+      const matchesBrand = !filters.brand.length || filters.brand.includes(product.product.brand);
 
-    if (sortOption === 'high-to-low') {
-      sorted.sort((a, b) => b.price - a.price);
-    } else if (sortOption === 'low-to-high') {
-      sorted.sort((a, b) => a.price - b.price);
-    } else if (sortOption === 'newest') {
-      sorted.sort((a, b) => new Date(b.createDate) - new Date(a.createDate));
-    }
+      const createDate = new Date(product.createDate);
+      const isNewest = createDate > new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+      const matchesNewest = !filters.newest || isNewest;
 
-    return sorted;
-  };
+      return matchesSearchQuery && matchesGender && matchesBrand && matchesNewest;
+    })
+    .sort((a, b) => {
+      if (filters.highToLow) {
+        return b.price - a.price;
+      }
+      if (filters.lowToHigh) {
+        return a.price - b.price;
+      }
+      if (filters.newest) {
+        return new Date(b.createDate) - new Date(a.createDate);
+      }
+      return 0;
+    });
 
-  const filteredProducts = sortedProducts().filter(product => {
-    const searchQueryLower = searchQuery.toLowerCase();
-
-    const matchesTitle = product.title?.toLowerCase().includes(searchQueryLower);
-    const matchesBrand = product.product.brand?.toLowerCase().includes(searchQueryLower);
-    const matchesCustomer = product.customerName?.toLowerCase().includes(searchQueryLower);
-    return matchesTitle || matchesBrand || matchesCustomer;
-  });
+    const slideBack = () => {
+      setFilterOpen(false);
+    };
 
   return (
-    <div className="App">
+    <div className={`App ${isFilterOpen ? 'filter-open' : ''}`}>
       <main className="App-content">
         <div className="search-filter-wrapper">
           <div className="search-controls">
@@ -69,56 +150,19 @@ function Home() {
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
               />
-            </div>
-          </div>
-          <div className="sort-controls-container">
-            <div className="sort-controls">
-              <label>
-                <input
-                  type="radio"
-                  name="sort"
-                  value=""
-                  checked={sortOption === ''}
-                  onChange={handleSortChange}
-                />
-                <span>Nothing</span>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="sort"
-                  value="high-to-low"
-                  checked={sortOption === 'high-to-low'}
-                  onChange={handleSortChange}
-                />
-                <span>Price: High - Low</span>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="sort"
-                  value="low-to-high"
-                  checked={sortOption === 'low-to-high'}
-                  onChange={handleSortChange}
-                />
-                <span>Price: Low - High</span>
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="sort"
-                  value="newest"
-                  checked={sortOption === 'newest'}
-                  onChange={handleSortChange}
-                />
-                <span>Newest</span>
-              </label>
+              <button className="filter-button" onClick={toggleFilter}>
+                Filter
+              </button>
             </div>
           </div>
         </div>
 
         {filteredProducts.length === 0 && searchQuery && (
           <div className="no-products">No products found</div>
+        )}
+
+        {filteredProducts.length === 0 && !searchQuery && (
+          <div className="no-products">No products match the selected filters</div>
         )}
 
         <ProductGallery
@@ -128,6 +172,77 @@ function Home() {
       </main>
       <Footer />
       <LoginModal isOpen={isModalOpen} onClose={toggleModal} />
+
+      <div className={`filter-menu ${isFilterOpen ? 'open' : ''}`}>
+        <div className="filter-header">
+          <h3>Filter Options</h3>
+          <button className="clear-button" onClick={clearFilters}>Clear</button>
+        </div>
+        <div className="filter-options">
+          <h4>Gender</h4>
+          {genders.map(gender => (
+            <label key={gender}>
+              <input
+                type="checkbox"
+                name="gender"
+                value={gender}
+                checked={filters.gender.includes(gender)}
+                onChange={handleCheckboxChange}
+              />
+              {gender.charAt(0).toUpperCase() + gender.slice(1)}
+            </label>
+          ))}
+
+          <h4>Sort By</h4>
+          <label>
+            <input
+              type="checkbox"
+              name="newest"
+              checked={filters.newest}
+              onChange={handleCheckboxChange}
+            />
+            Newest
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="sort"
+              value="highToLow"
+              checked={filters.highToLow}
+              onChange={handleCheckboxChange}
+            />
+            Price: High - Low
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="sort"
+              value="lowToHigh"
+              checked={filters.lowToHigh}
+              onChange={handleCheckboxChange}
+            />
+            Price: Low - High
+          </label>
+
+          <h4>Brand</h4>
+          {brands.map((brand) => (
+            <label key={brand}>
+              <input
+                type="checkbox"
+                name="brand"
+                value={brand}
+                checked={filters.brand.includes(brand)}
+                onChange={handleCheckboxChange}
+              />
+              {brand}
+            </label>
+          ))}
+        </div>
+        <FaArrowAltCircleLeft className="arrow-icon" onClick={slideBack} />
+
+      </div>
+
+      {isFilterOpen && <div className="overlay" onClick={toggleFilter}></div>}
     </div>
   );
 }
